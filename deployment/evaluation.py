1cf8fe167fbf84c37e783bf63487bf0b7b33fffa
QING PO WU WU <140480316+qingpowuwu@users.noreply.github.com>
2024-04-05 00:20:10 +0800

    Update evaluation.py
    加上了一些注释：这个代码是怎么运行的 => 主要是输入 之前用 generate.py 生成的 train_{idx}.py 文件 进 execute_script => 得到结果日志(log) => 提取 model 算出来的 metrics

--- deployment/evaluation.py
+++ deployment/evaluation.py
@@ -33,16 +33,20 @@ if __name__ == '__main__':
     # Evaluate all the tasks.
     for task in tasks_to_evaluate:
         
-        # Create a workspace
-        work_dir = f"./workspace/{args.path}/{task}"
+        # Create a workspace (当前的工作目录，我们做的实验都会放到这个目录里面)
+        work_dir = f"./workspace/{args.path}/{task}"  # ./workspace/gpt-3.5-turbo-16k_False_0/electricity
         if not os.path.exists(work_dir):
             os.makedirs(work_dir)
             
+        # ============= 将指定任务的环境文件 (xxx/benchmarks/electricity/env ) & 代码文件复制到 工作目录 (work_dir) 中 ==============
+        # (1)将指定任务的环境文件 (xxx/benchmarks/electricity/env ) 到 工作目录 (work_dir) 中
         if os.path.exists(f"../development/MLAgentBench/benchmarks/{task}"):
             shutil.copytree(f"../development/MLAgentBench/benchmarks/{task}/env", work_dir, symlinks=True, dirs_exist_ok=True)
-        
+            
+        # (2)将 generate.py 生成的代码文件 (xxx/codes/gpt-3.5-turbo-16k_False_0/electricity) 复制到 工作目录 (work_dir) 中
         if os.path.exists(f"./codes/{args.path}/{task}"):
             shutil.copytree(f"./codes/{args.path}/{task}", work_dir, symlinks=True, dirs_exist_ok=True)
+         # ============= 将指定任务的环境文件 (xxx/benchmarks/electricity/env ) & 代码文件复制到 工作目录 (work_dir) 中 ==============
         
         # Find submission pattern
         line = None
@@ -59,16 +63,22 @@ if __name__ == '__main__':
         
         pattern = line.split("\"")[1].split(":")[0]
         
-        # Create result path
-        result_dir = f"results/{args.path}"
+        # 创建结果目录 (result path)
+        result_dir = f"results/{args.path}"          # xxx/results/gpt-3.5-turbo-16k_False_0
         if not os.path.exists(result_dir):
             os.makedirs(result_dir)
-        result_filename = f"{result_dir}/{task}.csv"
-        
-        for idx in range(args.trials):
-            filename = f"train_{idx}.py"
-        
+        result_filename = f"{result_dir}/{task}.csv" # xxx/results/gpt-3.5-turbo-16k_False_0/electricity.csv
+
+        # 遍历 args.trials => 开始不停的做实验, 对于每次试验:
+        # (1) 执行训练脚本。
+        # (2) 从输出中提取结果,如 MSE(均方误差)和 MAE(平均绝对误差)。
+        for idx in range(args.trials): 
+            filename = f"train_{idx}.py" # train_1.py (之前用 generate.py 生成的)
+            
+            # 运行 xxx.py 代码，并且输出日志（log）
             log = execute_script(filename, work_dir=work_dir, device=args.device)
+            
+             # 根据提交代码中的模式(pattern),从输出脚本(log)中提取结果,如 MSE 和 MAE。如果无法提取结果,则将结果设置为 -1.0,表示执行失败。
             if pattern in log:
                 if "MSE" in line and "MAE" in line:
                     results[0].append(float(log.split(pattern)[1].split(":")[1].split(",")[0]))
@@ -81,11 +91,15 @@ if __name__ == '__main__':
                     results[1].append(-1.0)
                 else:
                     results.append(-1.0)
+                    
+        # 如果有 MSE 和 MAE 两个指标,则将它们作为两列保存;否则,只保存一列结果。
         if "MSE" in line and "MAE" in line:
             results = pd.DataFrame(results)
         else:
             results = pd.DataFrame(results).transpose()
-        results.to_csv(result_filename, index=False, header=False)
+            
+        # 将结果保存为 CSV 文件。
+        results.to_csv(result_filename, index=False, header=False) 
         print("results")
         print("="*100)
             
